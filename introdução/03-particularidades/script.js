edf7e35d02348815b3bcf12808c9d832374860a4
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-15 15:10:18 -0300

    commit

---
+++ introdução/03-particularidades/script.js
@@ -0,0 +1,14 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+var a = 1;
+a = 'teste';
+console.log(a);
+
+/*
+01- O códiigo é execultado de cima para baixo;
+02- A linguagem é case sensitive;
+03- O ponto e vírgula a cada instrução é opcional(mas recomendado);
+04- Há comentários para uma linha e múltiplas linhas;
+*/
---
+++ introdução/04-variáveis/script.js
@@ -0,0 +1,11 @@
+/*
+01- As variáveis armazenam valores;
+02- Estes valores podem ser utilizados posteriormente
+03- No JavaScript é possivel criar variáveis de 3 formas;
+04- Podemos mudar o tipo da variável livremente (tipagem fraca);
+*/
+
+var teste = 1;
+console.log(teste);
+teste = 'Matheus';
+console.log(teste);
