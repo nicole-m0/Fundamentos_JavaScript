62986851fdc31281114d17559899b39ecce0b4ab
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-17 15:58:40 -0300

    commit

---
+++ praticas/exe01/script.js
@@ -0,0 +1,89 @@
+//Desafio 01;
+
+var nome = "Julia";
+var idade = 20;
+var cidade = "Copaíba";
+var estudante = true;
+
+console.log("----------Perfil 1----------")
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Cidade:", cidade);
+console.log("É estudante?", estudante);
+console.log("--------------------------")
+
+//Desafio 02;
+var MeuNome = 'Maria Alice';
+var comidaFavorita = 'Pizza';
+var numeroDaSorte = 7;
+var gostoDeProgramar = true;
+
+console.log("----------Perfil 2----------")
+console.log("Meu nome é:", MeuNome);
+console.log("Minha comida favorita é:", comidaFavorita);
+console.log("Meu número da sorte é", numeroDaSorte);
+console.log("Eu gosto de programar:", gostoDeProgramar);
+console.log("--------------------------")
+
+//Desafio 03;
+var numero = 10;
+var texto = "Aprendendo JavaScript!";
+var ligado = true
+
+console.log("----------Teste_Valores----------")
+console.log("Valor do número: ", numero);
+console.log("Texto: ", texto);
+console.log("Boolean: ", ligado);
+console.log("--------------------------")
+
+//Desafio 04;
+var Nome = "Ana";
+var Idade = 30;
+var altura = 1.65;
+var estudaProgramacao = true;
+
+console.log("----------Perfil 3----------")
+console.log("Nome: ", Nome);
+console.log("Idade: ", Idade);
+console.log("Altura: ", altura);
+console.log("Estuda Programação? ", estudaProgramacao);
+console.log("--------------------------")
+
+//Desafio 05;
+var filme = "Interestelar";
+var ano = 2014;
+var duracao = "169 minutos";
+var jaAssitiu = true;
+
+console.log("----------Filme----------");
+console.log("Filme: ", filme);
+console.log("Ano: ", ano);
+console.log("Duração: ", duracao);
+console.log("Já assistiu? ", jaAssitiu);
+console.log("--------------------------");
+
+//Desafio 06;
+var produto = "Teclado";
+var preco = 120;
+var estoque = 15;
+var disponivel = true;
+
+console.log("----------produto----------");
+console.log("Produto: ", produto);
+console.log("Preço: ", preco);
+console.log("Estoque: ", estoque);
+console.log("Disponível: ", disponivel);
+console.log("--------------------------");
+
+//Desafio 07;
+var Nomep = "Guerreiro";
+var nivel = 5;
+var vida = 100;
+var vivo = true;
+
+console.log("----------Personagem----------");
+console.log("Nome do Personagem: ", Nomep);
+console.log("Nível: ", nivel);
+console.log("vida: ", vida);
+console.log("Está Vivo? ", vivo);
+console.log("--------------------------");
---
+++ tipos_de_dados/02strings/script.js
@@ -0,0 +1,22 @@
+var nome = 'Matheus';
+var sobrenome = 'Lacerda';
+
+console.log(nome);
+console.log(typeof nome);
+
+var nomeCompleto = nome +  " " + sobrenome;
+
+console.log(nomeCompleto);
+
+console.log(typeof "bzk");
+console.log(typeof "1284374581")
+
+var frase = "Esta é uma frase complexa";
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.writeln('Ele disse: "olá"');
+
+console.log("Este nome: " + nome);
