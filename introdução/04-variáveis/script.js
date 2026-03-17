e07e9363eeae6e34da458dfa4d1e23c4b85bc5af
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-17 14:12:36 -0300

    commit

--- introdução/04-variáveis/script.js
+++ introdução/04-variáveis/script.js
@@ -3,9 +3,16 @@
 02- Estes valores podem ser utilizados posteriormente
 03- No JavaScript é possivel criar variáveis de 3 formas;
 04- Podemos mudar o tipo da variável livremente (tipagem fraca);
+05- Usar camelCase;
+06- var, const e let(declarar variáveis);
 */
 
 var teste = 1;
+var nome = "João";
+
 console.log(teste);
-teste = 'Matheus';
+
+teste = 'Zuzinha';
 console.log(teste);
+console.log(nome);
+
