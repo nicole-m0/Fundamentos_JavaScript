a2a7165dae62370cc66c04bd6b8be2d5ce6bfa54
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-24 14:03:37 -0300

    commit

---
+++ estruturas/est_repetiçãoWHILE/script.js
@@ -0,0 +1,23 @@
+var x = 0;
+
+while(x < 5){
+    console.log("testando repeptição " + x);
+    //incremento
+    x++;
+}
+
+var arr = ['teste', 'olá', 'a', 'b'];
+var y = 0;
+
+while(y <= 3){
+    console.log(arr[y]);
+    y++;
+}
+
+var palavra = "João";
+var i = 0;
+
+while(1 < 8){
+    console.log(palavra[i]);
+    i++;
+}
--- praticas/exe04/script.js
+++ praticas/exe04/script.js
@@ -13,16 +13,16 @@ var usuario = "Maria";
 var bloqueado = false;
 
 if (!bloqueado){
-    console.log("Você está bloquedo(a)");
+    console.log("Acesso permitido");
 } else {
-    console.log("Acesso permitido!");
+    console.log("Acesso negado!");
 }
 
 //desafio 03
 var idade = 16;
-var temAutorização = true;
+var temAutorizacao = true;
 
-if (idade >= 18 || temAutorização == true){
+if (idade >= 18 || temAutorizacao == true){
     console.log("Você está autorizado a utilizar o sistema");
 } else {
     console.log("Você não está autorizado ao acesso.");
@@ -32,7 +32,7 @@ if (idade >= 18 || temAutorização == true){
 var saldo = 50;
 var preco = 60;
 
-if(saldo !== preco && saldo >= preco){
+if(saldo >= preco){
     console.log("pode comprar");
 } else {
     console.log("não pode comprar");
@@ -46,7 +46,7 @@ if (usuario === "admin" && senha === 1234){
     console.log("Login OK");
 }else if(usuario === "admin" && senha !== 1234){
     console.log("Senha incorreta. Tente novamente!");
-}else if (usuario !== "admin"){
+}else {
     console.log("usuário inválido!");
 }
 
@@ -75,9 +75,9 @@ if(temperatura >= 38 || doente){
 // desafio 08
 var usuario = {
     nome: "carlos",
-    idade: 15,
+    idade: 17,
     premium: false,
-    ativo: false,
+    ativo: true,
 }  
 
 if (usuario.ativo && usuario.premium){
