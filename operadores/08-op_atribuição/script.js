f1b04466d48dc81ee921da2fb634347d7feab8a2
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-24 14:44:56 -0300

    commit

--- operadores/08-op_atribuição/script.js
+++ operadores/08-op_atribuição/script.js
@@ -22,3 +22,12 @@ while(x <= 100){
     console.log(x);
     x *= 2;
 }
+
+console.log(x);
+
+var j = 5;
+
+while(x > 0){
+    console.log(x);
+    x -= j;
+}
---
+++ praticas/exe05/script.js
@@ -0,0 +1,23 @@
+// desafio 01
+var contador = 0;
+
+while(contador < 5){
+    console.log("contador "+contador);
+    contador += 1;
+}
+console.log("-----------------------------");
+// desafio 02
+var contador2 = 0;
+
+while(contador2 <= 10){
+    console.log("contador "+contador2);
+    contador2 += 2;
+}
+console.log("-----------------------------");
+//desafio 03
+var energia = 0;
+
+while(energia <= 10){
+    console.log(energia);
+    energia -= 1;
+}
