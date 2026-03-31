bd0a31babfbf4d8255f6a004c621c2bccadcd13b
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-31 16:06:04 -0300

    commit

--- praticas/exe05/script.js
+++ praticas/exe05/script.js
@@ -21,11 +21,11 @@ var energia = 10;
 
 while(energia >= 0){
     console.log(energia);
-    energia -= 1;
 
     if (energia === 0){
         console.log("Sem energia.")
     }
+    energia -= 1;
 }
 
 console.log("-----------------------------");
@@ -34,7 +34,9 @@ var soma = 0;
 var numero = 1;
 
 while(numero <= 5){
-    console.log(soma += numero);
+    soma += numero;
+    console.log(soma);
+    numero += 1; 
 }
 
 console.log("-----------------------------");
@@ -43,6 +45,56 @@ var resultado = 1;
 var numero = 1;
 
 while(numero <= 5){
-    console.log(numero *= resultado);
-
+    resultado *= numero;
+    console.log(resultado);
+    numero += 1;
 }
+
+console.log("-----------------------------");
+// desafio 06
+var vida = 10;
+
+while(vida >= 0){
+    console.log("Vida atual: "+vida);
+
+    if(vida === 0){
+        console.log("Personagem morreu!")
+    }
+    vida -= 2;
+}
+
+console.log("-----------------------------");
+// desafio 07
+var saldo = 100;
+
+while(saldo >= 0){
+    console.log("Seu saldo atual é: "+saldo);
+
+    if(saldo === 0){
+        console.log("Saldo esgotado!")
+    }
+    saldo -= 25;
+}
+
+console.log("-----------------------------");
+// desafio 08
+var nivel = 1;
+var xp = 0;
+
+while(nivel <= 5){
+    xp += 10;
+    console.log("Nível: "+nivel, " || XP: "+xp);
+    nivel += 1;
+}
+
+console.log("-----------------------------");
+// desafio 09
+var vida = 20;
+var dano = 2;
+
+while(vida > 0){
+    console.log(vida);
+    vida -= dano;
+    dano += 1;
+}
+console.log("inimigo derrotado!");
